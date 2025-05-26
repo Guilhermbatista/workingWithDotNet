e6451629fc47755b1161a46139d1defb0bb20b79
Guilhermbatista <guilherme.santos201412@gmail.com>
2025-05-26 20:55:42 -0300

    building API RestFull

---
+++ CatalogAPI/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CatalogAPI.Models;
+
+[Table("Products")]
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    [StringLength(80)]
+    public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? Description { get; set; }
+    [Required]
+    [Column(TypeName ="deciman(10,2)")]
+    public decimal Price { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? ImgURL { get; set; }
+    public float Stock { get; set; }
+    public DateTime DateRegister { get; set; }
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+
+}
